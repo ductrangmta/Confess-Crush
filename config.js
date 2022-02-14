e95d9b4989e481e9da86de6e98e418a59500b529
ductrangmta <99657090+ductrangmta@users.noreply.github.com>
2022-02-14 19:38:16 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Gửi đến vợ yêu của anh",
+    introTitle: 'Bé ủn ỉn của anh nè',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -8,13 +8,13 @@ const CONFIG = {
     btnIntro: '^^HiHi^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
+    btnYes: 'Yêu lắm <33',
+    btnNo: 'Không thèm :3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao em lại yêu mình anh <3',
+    btnReply: 'Gửi anh yêu <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
+    mess: 'Anh biết mà 🥰. Yêu vợ của anh nhiều lắm 😘😘',
+    messDesc: 'Tình yêu của anh, ngày lễ tình nhân thật tuyệt vời nhé.',
+    btnAccept: 'Yêu anh <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
